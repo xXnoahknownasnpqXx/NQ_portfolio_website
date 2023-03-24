bdcea21fb8ef210a8bc3d81701019d6731dcf0c1
xXnoahknownasnpqXx <noahknownasnpq@gmail.com>
2023-03-24 09:30:24 -0400

    Update across all pages and CSS and JS, more added to Hobbies page

--- js/navbar.js
+++ js/navbar.js
@@ -1,9 +1,9 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function responsiveNavBar() {
-    var x = document.getElementById("nav_bar_responsive");
-    if (x.className === "nav_bar") {
-      x.className += " responsive";
+    var navBar = document.getElementById("responsive_navBar");
+    
+    if (navBar.className === "nav_bar") {
+      navBar.className += " responsive";
     } else {
-      x.className = "nav_bar";
+      navBar.className = "nav_bar";
     }
   } 
